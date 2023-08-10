df09c2334a018b32762685926cae4013b2275700
TeKrop <tekrop@gmail.com>
2023-08-11 01:49:24 +0200

    Added new flashpoint gamemode and new maps

--- tests/views/test_maps_route.py
+++ tests/views/test_maps_route.py
@@ -40,10 +40,10 @@ def test_get_maps_invalid_gamemode():
             {
                 "type": "enum",
                 "loc": ["query", "gamemode"],
-                "msg": "Input should be 'assault','capture-the-flag','control','deathmatch','elimination','escort','hybrid','push' or 'team-deathmatch'",
+                "msg": "Input should be 'assault','capture-the-flag','control','deathmatch','elimination','escort','flashpoint','hybrid','push' or 'team-deathmatch'",
                 "input": "invalid",
                 "ctx": {
-                    "expected": "'assault','capture-the-flag','control','deathmatch','elimination','escort','hybrid','push' or 'team-deathmatch'"
+                    "expected": "'assault','capture-the-flag','control','deathmatch','elimination','escort','flashpoint','hybrid','push' or 'team-deathmatch'"
                 },
             }
         ]
