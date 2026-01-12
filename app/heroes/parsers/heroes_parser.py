32aacbc4b48ec3e176d7c2b0777809324c055316
Valentin Porchet <tekrop@proton.me>
2026-01-12 21:27:42 +0100

    --wip-- [skip ci]

--- app/heroes/parsers/heroes_parser.py
+++ app/heroes/parsers/heroes_parser.py
@@ -1,6 +1,7 @@
 """Heroes page Parser module"""
 
 from app.config import settings
+from app.exceptions import ParserParsingError
 from app.parsers import HTMLParser
 
 
@@ -10,20 +11,21 @@ class HeroesParser(HTMLParser):
     root_path = settings.heroes_path
 
     async def parse_data(self) -> list[dict]:
-        return sorted(
-            [
+        heroes = []
+        for hero in self.root_tag.css("div.heroIndexWrapper blz-media-gallery a"):
+            if not hero.attributes["href"]:
+                msg = "Invalid hero URL"
+                raise ParserParsingError(msg)
+            heroes.append(
                 {
                     "key": hero.attributes["href"].split("/")[-1],
                     "name": hero.css_first("blz-card blz-content-block h2").text(),
                     "portrait": hero.css_first("blz-card blz-image").attributes["src"],
                     "role": hero.attributes["data-role"],
                 }
-                for hero in self.root_tag.css(
-                    "div.heroIndexWrapper blz-media-gallery a"
-                )
-            ],
-            key=lambda hero: hero["key"],
-        )
+            )
+
+        return sorted(heroes, key=lambda hero: hero["key"])
 
     def filter_request_using_query(self, **kwargs) -> list[dict]:
         role = kwargs.get("role")
