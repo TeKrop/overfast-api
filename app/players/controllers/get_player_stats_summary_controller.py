38789caed51bf67463207e9488056bea6fe86a20
Valentin Porchet <tekrop@proton.me>
2026-02-08 18:02:08 +0100

    fixed SonarQube alert

--- app/players/controllers/get_player_stats_summary_controller.py
+++ app/players/controllers/get_player_stats_summary_controller.py
@@ -20,8 +20,7 @@ from .base_player_controller import BasePlayerController
 
 class GetPlayerStatsSummaryController(BasePlayerController):
     """Player Stats Summary Controller used in order to retrieve essential
-    stats of a player, often used for tracking progress : winrate, kda, damage, etc.
-    Using the PlayerStatsSummaryParser.
+    stats of a player, often used for tracking progress: winrate, kda, damage, etc.
     """
 
     parser_classes: ClassVar[list] = []
